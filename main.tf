11ccdc63a6fb5a23b914b4d9009f58de492fa508
Mukund Bajaj <mukund.bajaj@neudesic.com>
2024-10-01 15:42:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
+  tags = {
+      Owner = "Mukund Bajaj"
+      Purpose = "Testing for Jenkins"
+  }
 }
