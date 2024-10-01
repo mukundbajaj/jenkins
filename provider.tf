147ec7cd04b03689d70a0d59cccb54e922e580d6
Mukund Bajaj <mukund.bajaj@neudesic.com>
2024-10-01 13:38:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,8 +21,7 @@ provider "azurerm" {
   features {}
   # tenant_id           = // ARM_TENANT_ID
   # subscription_id     = // ARM_SUBSCRIPTION_ID
-  # use_msi             = // ARM_USE_MSI (set to true for system/user assigned managed identity)
+  use_msi             = trie
   # client_id           = // ARM_CLIENT_ID, appId for service principal or user assigned managed identity)
   # client_secret       = // ARM_CLIENT_SECRET, password for service principal
-  storage_use_azuread = true
 }
